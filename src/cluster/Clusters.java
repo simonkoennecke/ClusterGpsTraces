330307365e14df377e3b62d759d04bc8cf384fa5
Simon Koennecke <simonkoennecke+github@gmail.com>
2013-03-26 17:11:24 +0100

    für die übersicht bei k means bei den iterativen schritten

---
+++ src/cluster/Clusters.java
@@ -0,0 +1,10 @@
+package cluster;
+
+import java.util.ArrayList;
+
+public class Clusters {
+	public ArrayList<Cluster> list = new ArrayList<Cluster>();
+	public int size(){
+		return list.size();
+	}
+}
